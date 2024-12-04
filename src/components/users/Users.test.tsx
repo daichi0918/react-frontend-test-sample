c3ab1cdfa892751f1ce0ddca4c567a9d9c22fe4e
daichi0918 <kayasugadaichi@icloud.com>
2024-12-04 14:38:21 +0900

    fin:chap57

--- src/components/users/Users.test.tsx
+++ src/components/users/Users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { Users } from './Users';
+import { server } from '../../mocks/server';
+import { rest } from 'msw';
 
 describe('Users', () => {
   test('renders collectly', () => {
@@ -8,9 +10,23 @@ describe('Users', () => {
     expect(textElement).toBeInTheDocument();
   });
 
-  test('renders a list of users', async () => {
+  // test('renders a list of users', async () => {
+  //   render(<Users />);
+  //   const users = await screen.findAllByRole('listitem');
+  //   expect(users).toHaveLength(10);
+  // });
+
+  test('renders error', async () => {
+    server.use(
+      rest.get(
+        'https://jsonplaceholder.typicode.com/users',
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
     render(<Users />);
-    const users = await screen.findAllByRole('listitem');
-    expect(users).toHaveLength(10);
+    const error = await screen.findByText('Error Fetching users');
+    expect(error).toBeInTheDocument();
   });
 });
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,19 @@
+import { rest } from 'msw';
+export const handlers = [
+  rest.get(
+    'https://jsonplaceholder.typicode.com/users',
+    (req: any, res: any, ctx: any) => {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          {
+            name: 'ShinCode',
+          },
+          {
+            name: 'Test',
+          },
+        ])
+      );
+    }
+  ),
+];
